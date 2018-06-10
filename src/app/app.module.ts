42b69f014bb9389661d92c7d91b5e6f708062fb2
Tomás <tomas.botella@gmail.com>
2018-06-10 19:02:52 +0200

    Create main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserAccountComponent } from './user-account/user-account.component';
+import { UserAccountCreateComponent } from './user-account-create/user-account-create.component';
+import { MatchListComponent } from './match-list/match-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAccountComponent,
+    UserAccountCreateComponent,
+    MatchListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/user-account-create/user-account-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-account-create',
+  templateUrl: './user-account-create.component.html',
+  styleUrls: ['./user-account-create.component.css']
+})
+export class UserAccountCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
